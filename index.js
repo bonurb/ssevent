8d966e840a2154c3ba2ea8ec3c3875e88d49b38c
bonurb <bonurb@t-online.de>
2023-05-05 13:13:13 +0200

    mit pug

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var cp = require("child_process"),
          express = require("express"),
          app = express();
+var path = require('path');
 
 app.use('/', express.static(__dirname));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 let sseResult;
 let spw;
 let str;
 
+app.get('/', function(req, res){
+    console.log('/ ...');
+    res.render('index');
+});
+
 app.get('/msg', function(req, res){
-    console.log('/msg ...')
+    console.log('/msg ...');
     sseResult = res;
     sseResult.writeHead(200, { "Content-Type": "text/event-stream",
                          "Cache-control": "no-cache" });
@@ -18,7 +26,7 @@ app.get('/msg', function(req, res){
 
 app.get('/spawn', function(req, res){
     console.log('/spawn ...')
-    spw = cp.spawn('ping', ['-n', '10', '127.0.0.1']),
+    spw = cp.spawn('ping', ['-n', '3', '127.0.0.1']),
     str = "";
 
     spw.stdout.on('data', function (data) {
@@ -41,7 +49,7 @@ app.get('/spawn', function(req, res){
 
     spw.on('close', function (code) {
         sseResult.write('spawn closed\n\n');
-        sseResult.end('spawn closed');
+        // sseResult.end('spawn closed');
     });
 
     spw.stderr.on('data', function (data) {
