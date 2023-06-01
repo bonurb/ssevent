b96796b788a42828893a535ac9b0837e360444c6
bonurb <bonurb@t-online.de>
2023-06-01 10:55:30 +0200

    kill ok

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
 var cp = require("child_process"),
          express = require("express"),
          app = express();
-var path = require('path');
 
 app.use('/', express.static(__dirname));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 let sseResult;
 let spw;
 let str;
 
-app.get('/', function(req, res){
-    console.log('/ ...');
-    res.render('index');
-});
-
 app.get('/msg', function(req, res){
-    console.log('/msg ...');
+    console.log('/msg ...')
     sseResult = res;
     sseResult.writeHead(200, { "Content-Type": "text/event-stream",
                          "Cache-control": "no-cache" });
 
 });
 
-app.get('/prespawn', function(req, res){
-    console.log('/prespawn ...')
-    res.render('prespawn');
+app.get('/kill', function(req, res){
+    console.log('/kill ...');
+    spw.kill('SIGINT');
+});
+
+app.get('/ping', function(req, res){
+    console.log('/ping ...');
+    // spw.stdin.write('stdbuf -i0 -o0 -e0 ll \n');
+    spw.stdin.write('ping localhost \n');
+    // res.end();
+});
+
+app.get('/ll', function(req, res){
+    console.log('/ll ...');
+    spw.stdin.write('stdbuf -i0 -o0 -e0 ls -al \n');
+    // res.end();
 });
 
 app.get('/spawn', function(req, res){
-    console.log('/spawn ...')
-    spw = cp.spawn('ping', ['-n', '3', '127.0.0.1']),
+    console.log('/spawn ...');
+    // spw = cp.spawn('ping', ['-n', '100', '127.0.0.1']);
+    spw = cp.spawn("c:/brtmp/prog/PortableGit/bin/bash.exe");
     str = "";
-
+    
     spw.stdout.on('data', function (data) {
         str += data.toString();
-
+        
         // just so we can see the server is doing something
         console.log("data");
-
+        
         // Flush out line by line.
         var lines = str.split("\n");
         for(var i in lines) {
@@ -51,16 +57,17 @@ app.get('/spawn', function(req, res){
             }
         }
     });
-
+    
     spw.on('close', function (code) {
+        console.log('spw closed ...');
         sseResult.write('spawn closed\n\n');
-        // sseResult.end('spawn closed');
+        sseResult.end('spawn closed');
     });
-
+    
     spw.stderr.on('data', function (data) {
         sseResult.end('stderr: ' + data);
     });
-    // res.render('spawn');
+    // res.end();
 });
 
 app.listen(3001);
